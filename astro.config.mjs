7197477e495532f81a9ef9fb244a584ba9442657
Rayyd <57541984+Rayyd@users.noreply.github.com>
2025-09-12 20:18:32 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-// IMPORTANT: GitHub Pages base path
-const site = "https://rayyd.github.io/astro-site-starter/";
+const isVercel = process.env.VERCEL === "1";
+
+// IMPORTANT: laissez TOUJOURS un slash final pour la base non-root
+const site = isVercel
+  ? `https://${process.env.VERCEL_URL || "localhost:4321"}/`
+  : "https://rayyd.github.io/astro-site-starter/"; // <- slash final
 
 export default defineConfig({
   site,
-  base: "/astro-site-starter",
+  base: isVercel ? "/" : "/astro-site-starter/", // <- slash final
   integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
   output: "static",
-  prefetch: { defaultStrategy: "viewport" }
+  prefetch: { defaultStrategy: "viewport" },
 });
